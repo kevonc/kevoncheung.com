8a0e7b955aeba58af5cd4ceadb0256a6dfe486ba
Kevon Cheung <kevon.cheung@gmail.com>
2025-01-17 14:53:55 +0800

    blog post is now at root url

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -7,20 +7,12 @@ self.__BUILD_MANIFEST = {
   ],
   "ampDevFiles": [],
   "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/webpack.js",
-    "static/chunks/main-app.js"
-  ],
+  "rootMainFiles": [],
   "pages": {
     "/": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/index.js"
-    ],
-    "/[slug]": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/[slug].js"
     ],
     "/_app": [
       "static/chunks/webpack.js",
@@ -33,10 +25,20 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
+    "/about": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/about.js"
+    ],
     "/blog": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/blog.js"
+    ],
+    "/now": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/now.js"
     ]
   },
   "ampFirstPages": []
