94e1230ce8b04006b54e67e3dc03b13102b35044
Kevon Cheung <kevon.cheung@gmail.com>
2025-01-26 22:01:33 +0800

    fix sub box

--- components/SubscriptionBox.js
+++ components/SubscriptionBox.js
@@ -2,13 +2,9 @@ import Script from 'next/script'
 
 export default function SubscriptionBox() {
   return (
-    <div className="my-16">
-      <div id="fca4203871" />
-      <Script
-        src="https://smallschool.kit.com/fca4203871/index.js"
-        data-uid="fca4203871"
-        strategy="afterInteractive"
-      />
-    </div>
+    <script async data-uid="fca4203871" src="https://smallschool.kit.com/fca4203871/index.js"></script>
   )
 } 
+
+
+
