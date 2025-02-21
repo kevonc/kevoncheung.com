3dd8409c23f21162598ff6bfad4b8be4127ad701
Kevon Cheung <kevon.cheung@gmail.com>
2025-02-21 11:56:46 +0800

    update 404 page

--- pages/404.js
+++ pages/404.js
@@ -13,8 +13,8 @@ export default function Custom404() {
           alt="404" 
           className="mb-10 w-[480px] h-[252px]" 
         />
-        <h1 className="text-3xl font-bold mb-4">Aye aye! What you're looking for doesn't seem to exist anymore.</h1>
-        <h2 className="text-xl font-semibold mb-12">Maybe start here instead?</h2>
+        <h1 className="text-3xl font-bold mb-4">Ay ay! What you're looking for doesn't seem to exist.</h1>
+        <h2 className="text-xl font-semibold mb-12">Maybe start here?</h2>
         <Link 
           href="/about" 
           className="bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-green-700 transition-colors"
